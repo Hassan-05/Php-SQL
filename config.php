27d265bd73c9291cea0751aa893c54066d2f252c
Hassan_onPc <hassanshahbaz016@gmail.com>
2024-10-14 12:47:04 +0500

    error_log.txt to errors.log

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 
 function customErrorHandler($errno, $errstr, $errfile, $errline) {
     $errorMessage = "Error [$errno]: $errstr in $errfile on line $errline \n";
-    error_log($errorMessage . PHP_EOL, 3, 'error_log.txt');
+    error_log($errorMessage . PHP_EOL, 3, 'errors.log');
     echo "Sorry an error accured";
     return true;
 }
@@ -13,6 +13,6 @@ set_error_handler('customErrorHandler');
 // Disable displaying errors to the browser
 ini_set('display_errors', '0');
 ini_set('log_errors', '1');
-ini_set('error_log','error_log.txt');
+ini_set('error_log','errors.log');
 error_reporting(E_ALL);
 ?>
